55d907d2402cdf2eccf7e3d83bf4443f520b2608
turtel42 <papadimitrios216@gmail.com>
2024-03-18 15:09:02 +0200

    added contacts but still work in progress

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#01303f',
+        text: '#d4f0fc',
+        subtext: '#ffffff',
+        titles: '#02577a',
+        links: '#89d6fb'
+      },
+      fontFamily: {
+        body: ['Inter']
+      }
+    },
+  },
+  plugins: [],
+}
+
