2ea574ee6ffd9cc7918568e4d9f4bc260243bc8d
turtel42 <papadimitrios216@gmail.com>
2024-03-18 08:22:31 +0200

    first draft of web page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./public/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#01303f',
+        text: '#d4f0fc',
+        subtext: '#ffffff',
+        titles: '#02577a',
+        links: '#89d6fb'
+      },
+      fontFamily: {
+        body: ['Inter']
+      }
+    },
   },
   plugins: [],
 }
